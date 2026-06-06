ff44cb58ed91591eb599676d1186b5583a0876fa
sevdesevi <svsila16@gmail.com>
2026-06-07 00:09:11 +0300

    Update rulet.py

--- rulet.py
+++ rulet.py
@@ -5,7 +5,7 @@ kurban_listesi=kurban_listesi
 def rus_ruletimiz(kl=6,sayac=1,r=random.randint(1,6)):
     print("ss subayı silahını çıkardı ve rus ruleti için hazırladı.")
     print(f"{sayac}. kişinin o an ölme ihtimali={1/kl}")
-    print(f"{sayac}. kişinin genel ölme ihtimali={1/kl*kl/6}'di") #BURAK ŞAHİN KODLADI
+    print(f"{sayac}. kişinin genel ölme ihtimali={1/kl*kl/6}'di")
     #eğer patlarsa çık
     if r==sayac:
         print(f"boom ..... {sayac}.kişi öldü")
